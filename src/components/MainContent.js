0d1e4c329f61ebef654b9b4fb960a557d63d6906
mani <jmkmani55@kpostindia.com>
2025-03-25 19:01:25 +0530

    added dashboard static image and adjusted sidebar width while collapsed.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,5 +1,10 @@
+import React from "react";
+import DashboardImg from "../assets/images/Dashboard.png";
+
 const Dashboard = () => {
-  return <div className="content">Welcome to Dashboard</div>;
+  return <div className="content">
+    <img src={DashboardImg} alt="Image" style={{height:" 100%", width:"100%"}} />
+  </div>;
 };
 
 export default Dashboard;
